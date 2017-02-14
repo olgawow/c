7d9aebaf62e962d72e9a74d589d17cb4c57533bb
Olga Oleksyuk <ovoitovych@ezcater.com>
2017-02-14 09:18:37 -0500

    wip

---
+++ array.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  array.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 09:15:59
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  OLGA OLEKSYUK (olgawow), mail@olga-v.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+
+void main() {
+
+}
+
